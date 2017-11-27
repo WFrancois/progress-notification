7a27e83aa98461c9272c35378f252dfb6e2f8e16
WFrancois <wroblewski.francois@gmail.com>
2017-11-27 15:05:08 +0100

    Add log

--- app/Controller/SubmitController.php
+++ app/Controller/SubmitController.php
@@ -74,31 +74,38 @@ class SubmitController extends BaseController
         Log::add('receive-payload', ['payload' => $payload]);
 
         if ($payload['type'] !== 'guild_watch_buffered' && $payload['type'] !== 'guild_watch') {
+            Log::add('incorrect-type', ['payload' => $payload]);
             return $response->withJson(['error' => 'incorrect-type'])->withStatus(400);
         }
 
         if ($payload['raidId'] != 8638) {
+            Log::add('incorrect-raid', ['payload' => $payload]);
             return $response->withJson(['error' => 'incorrect-raid'])->withStatus(400);
         }
 
         if ($payload['difficulty'] !== 'mythic') {
+            Log::add('incorrect-difficulty', ['payload' => $payload]);
             return $response->withJson(['error' => 'incorrect-difficulty'])->withStatus(400);
         }
 
         if (empty($payload['payloadParams']) || empty($payload['payloadParams']['boss_ranks']['world']) ||
             empty($payload['payloadParams']['boss_ranks']['world'])) {
+            Log::add('missing-rank', ['payload' => $payload]);
             return $response->withJson(['error' => 'missing-rank'])->withStatus(400);
         }
 
         if (empty($payload['payloadParams']['guild'])) {
+            Log::add('missing-guild', ['payload' => $payload]);
             return $response->withJson(['error' => 'missing-guild'])->withStatus(400);
         }
 
         if (empty($payload['bossId']) || empty(Util::getBossName(intval($payload['bossId'])))) {
+            Log::add('incorrect-boss-id', ['payload' => $payload]);
             return $response->withJson(['error' => 'incorrect-boss-id'])->withStatus(400);
         }
 
         if(empty($payload['payloadParams']['region'])) {
+            Log::add('missing-region', ['payload' => $payload]);
             return $response->withJson(['error' => 'missing-region'])->withStatus(400);
         }
 
@@ -133,6 +140,7 @@ class SubmitController extends BaseController
         $channel->exchange_declare('router', 'direct', false, true, false);
         $channel->queue_bind('notification', 'router');
 
+        Log::add('send-notification', ['payload' => $payload, 'subscriber' => count($subscribers)]);
         foreach ($subscribers as $subscriber) {
             $messageBroker = [
                 'pushInfo' => \json_decode($subscriber['google_json'], true),
--- app/Service/Log.php
+++ app/Service/Log.php
@@ -13,6 +13,7 @@ class Log
 {
     public static function add(string $type, array $data)
     {
+        $data['server'] = $_SERVER;
         PDO::getInstance()->insert(['log_type' => $type, 'log_data' => \json_encode($data)])->into('log')->execute();
     }
 }
