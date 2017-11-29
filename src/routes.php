a4fcf43ab242cb65523329a9097a426f2d1a3cae
WFrancois <wroblewski.francois@gmail.com>
2017-11-29 11:39:08 +0100

    Remove About page, add github link, change to US/OC

--- src/routes.php
+++ src/routes.php
@@ -3,8 +3,6 @@
 
 $app->get('/', \ProgressNotification\Controller\RegisterController::class . ':registerAction')->setName('homePage');
 
-$app->get('/about', \ProgressNotification\Controller\AboutController::class)->setName('aboutPage');
-
 $app->post('/ajax/register', \ProgressNotification\Controller\RegisterController::class . ':ajaxRegister')->setName('registerAjax');
 
 $app->post('/submit-kill', \ProgressNotification\Controller\SubmitController::class . ':submitKill')->setName('submitKill');
