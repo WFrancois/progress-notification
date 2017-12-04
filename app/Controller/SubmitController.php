6c8854016fff1ba9cca16cceaf610b0956857eb7
WFrancois <wroblewski.francois@gmail.com>
2017-12-04 13:34:13 +0100

    Divise sending push api into a different function

--- app/Controller/SubmitController.php
+++ app/Controller/SubmitController.php
@@ -14,9 +14,10 @@ use Slim\Http\Response;
 
 class SubmitController extends BaseController
 {
+    private $payload = [];
+
     public function submitKill(Request $request, Response $response)
     {
-        $time = microtime(true);
         $rawPayload = file_get_contents("php://input");
         $sign = hash_hmac('md5', $rawPayload, Config::getInstance()->get('access_token'));
 
@@ -24,68 +25,77 @@ class SubmitController extends BaseController
             return $response->withJson(['error' => 'incorrect-access-token'])->withStatus(401);
         }
 
-        $payload = $request->getParams();
-        Log::add('receive-payload', ['payload' => $payload]);
+        $this->payload = $request->getParams();
 
-        if ($payload['type'] !== 'guild_progress_updated') {
-            Log::add('incorrect-type', ['payload' => $payload]);
+        Log::add('receive-payload', ['payload' => $this->payload]);
+
+        if ($this->payload['type'] !== 'guild_progress_updated') {
+            Log::add('incorrect-type', ['payload' => $this->payload]);
             return $response->withJson(['error' => 'incorrect-type'])->withStatus(400);
         }
 
-        if ($payload['raid']['id'] != 8638) {
-            Log::add('only-log-antorus', ['payload' => $payload]);
+        if ($this->payload['raid']['id'] != 8638) {
+            Log::add('only-log-antorus', ['payload' => $this->payload]);
             return $response->withJson(['error' => 'only-log-antorus']);
         }
 
-        if ($payload['difficulty'] !== 'mythic') {
-            Log::add('only-log-mythic', ['payload' => $payload]);
+        if ($this->payload['difficulty'] !== 'mythic') {
+            Log::add('only-log-mythic', ['payload' => $this->payload]);
             return $response->withJson(['error' => 'only-log-mythic']);
         }
 
-        if (empty($payload['bossRanks']) || empty($payload['bossRanks']['world']['new']) ||
-            empty($payload['bossRanks']['region']['new'])) {
-            Log::add('missing-rank', ['payload' => $payload]);
+        if (empty($this->payload['bossRanks']) || empty($this->payload['bossRanks']['world']['new']) ||
+            empty($this->payload['bossRanks']['region']['new'])) {
+            Log::add('missing-rank', ['payload' => $this->payload]);
             return $response->withJson(['error' => 'missing-rank'])->withStatus(400);
         }
 
-        if (empty($payload['guild']['name'])) {
-            Log::add('missing-guild', ['payload' => $payload]);
+        if (empty($this->payload['guild']['name'])) {
+            Log::add('missing-guild', ['payload' => $this->payload]);
             return $response->withJson(['error' => 'missing-guild'])->withStatus(400);
         }
 
-        if (empty($payload['boss']['id']) || empty(Util::getBossName(intval($payload['boss']['id'])))) {
-            Log::add('incorrect-boss-id', ['payload' => $payload]);
+        if (empty($this->payload['boss']['id']) || empty(Util::getBossName(intval($this->payload['boss']['id'])))) {
+            Log::add('incorrect-boss-id', ['payload' => $this->payload]);
             return $response->withJson(['error' => 'incorrect-boss-id'])->withStatus(400);
         }
 
-        if(empty($payload['guildProfileUrl'])) {
-            Log::add('missing-url', ['payload' => $payload]);
+        if(empty($this->payload['guildProfileUrl'])) {
+            Log::add('missing-url', ['payload' => $this->payload]);
             return $response->withJson(['error' => 'missing-url'])->withStatus(400);
         }
 
-        if(empty($payload['region']['slug'])) {
-            Log::add('missing-region', ['payload' => $payload]);
+        if(empty($this->payload['region']['slug'])) {
+            Log::add('missing-region', ['payload' => $this->payload]);
             return $response->withJson(['error' => 'missing-region'])->withStatus(400);
         }
 
-        if(empty($payload['region']['shortName'])) {
-            Log::add('missing-region-name', ['payload' => $payload]);
+        if(empty($this->payload['region']['shortName'])) {
+            Log::add('missing-region-name', ['payload' => $this->payload]);
             return $response->withJson(['error' => 'missing-region-short-name'])->withStatus(400);
         }
 
-        $bossId = (int)$payload['boss']['id'];
-        $rankWorld = (int)$payload['bossRanks']['world']['new'];
-        $rankRegion = (int)$payload['bossRanks']['region']['new'];
-        $guildName = $payload['guild']['name'];
-        $region = $payload['region']['slug'];
+        $bossId = (int)$this->payload['boss']['id'];
+        $rankWorld = (int)$this->payload['bossRanks']['world']['new'];
+        $rankRegion = (int)$this->payload['bossRanks']['region']['new'];
+        $guildName = $this->payload['guild']['name'];
+        $region = $this->payload['region']['slug'];
 
         $bossName = Util::getBossName($bossId);
-        if(!empty($payload['boss']['name'])) {
-            $bossName = $payload['boss']['name'];
+        if(!empty($this->payload['boss']['name'])) {
+            $bossName = $this->payload['boss']['name'];
         }
 
         $message = $guildName . ' killed ' . $bossName .
-            ' World ' . Util::getOrdinal($rankWorld) . ', ' . $payload['region']['shortName'] . ' ' . Util::getOrdinal($rankRegion);
+            ' World ' . Util::getOrdinal($rankWorld) . ', ' . $this->payload['region']['shortName'] . ' ' . Util::getOrdinal($rankRegion);
+
+
+        $this->sendPushApi($rankWorld, $rankRegion, $region, $bossId, $this->payload['guildProfileUrl'], $message);
+    }
+
+    private function sendPushApi($rankWorld, $rankRegion, $region, $bossId, $guildUrl, $message)
+    {
+        $time = microtime(true);
 
         $query = 'SELECT * FROM subscribers WHERE ';
 
@@ -102,7 +112,7 @@ class SubmitController extends BaseController
             'title' => 'Raid Progress Update!',
             'text' => $message,
             'icon' => '/img/' . $bossId . '.jpg',
-            'url' => $payload['guildProfileUrl'],
+            'url' => $guildUrl,
         ];
 
         $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
@@ -111,7 +121,7 @@ class SubmitController extends BaseController
         $channel->exchange_declare('router', 'direct', false, true, false);
         $channel->queue_bind('notification', 'router');
 
-        Log::add('send-notification', ['payload' => $payload, 'subscriber' => count($subscribers)]);
+        Log::add('send-notification', ['payload' => $this->payload, 'subscriber' => count($subscribers)]);
         foreach ($subscribers as $subscriber) {
             $messageBroker = [
                 'pushInfo' => \json_decode($subscriber['google_json'], true),
@@ -125,6 +135,6 @@ class SubmitController extends BaseController
         $channel->close();
         $connection->close();
 
-        Log::add('done', ['payload' => $payload, 'time' => microtime(true) - $time]);
+        Log::add('sent-push-api', ['payload' => $this->payload, 'time' => microtime(true) - $time]);
     }
 }
