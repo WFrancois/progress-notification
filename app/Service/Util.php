002c009297ab5b1cc617bb723e024d1a2e7c7eaf
WFrancois <wroblewski.francois@gmail.com>
2020-11-18 00:13:18 +0100

    Feature: Update for Castle Nathria

--- app/Service/Util.php
+++ app/Service/Util.php
@@ -25,18 +25,16 @@ class Util
     public static function getBossName(int $bossId): string
     {
         $bossIds = [
-            156818 => 'Wrathion, the Black Emperor',
-            156523 => 'Maut',
-            161901 => 'The Prophet Skitra',
-            160229 => 'Dark Inquisitor Xanesh',
-            157253 => 'The Hivemind',
-            157231 => 'Shad\'har the Insatiable',
-            157602 => 'Drest\'agath',
-            158328 => 'Il\'gynoth, Corruption Reborn',
-            157354 => 'Vexiona',
-            156866 => 'Ra-den the Despoiled',
-            157439 => 'Carapace of N\'Zoth',
-            158041 => 'N\'Zoth the Corruptor',
+            172145 => 'Shriekwing',
+            165066 => 'Huntsman Altimor',
+            164261 => 'Hungering Destroyer',
+            166644 => 'Artificer Xy\'Mox',
+            24664 => 'Sun King\'s Salvation',
+            167517 => 'Lady Inerva Darkvein',
+            166971 => 'The Council of Blood',
+            174733 => 'Sludgefist',
+            165318 => 'Stone Legion Generals',
+            168938 => 'Sire Denathrius',
         ];
 
         return $bossIds[$bossId] ?? '';
