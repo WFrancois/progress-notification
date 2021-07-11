6bf581dc73070212b896b576b780029b9d592429
WFrancois <wroblewski.francois@gmail.com>
2021-07-11 17:25:53 +0200

    Update first boss & current raid

--- app/Controller/SubmitController.php
+++ app/Controller/SubmitController.php
@@ -14,8 +14,8 @@ use Slim\Http\Response;
 
 class SubmitController extends BaseController
 {
-    private const FIRST_BOSS_ID = 172145;
-    private const CURRENT_RAID_ID = 13224;
+    private const FIRST_BOSS_ID = 152253;
+    private const CURRENT_RAID_ID = 13561;
 
     private $payload = [];
 
